86dd2d4191debbddd4dad04d8e2fc788d519792b
AvKov <mnrosyidas@gmail.com>
2025-12-04 23:16:59 +0700

    [Improve] Fix Performance cause by gradient

--- background.js
+++ background.js
@@ -1,3 +1,9 @@
-chrome.runtime.onInstalled.addListener(() => {
-  console.log("Extension installed");
+chrome.runtime.onInstalled.addListener((details) => {
+  if (details.reason === 'install') {
+    // Set extension to disabled by default on first install
+    chrome.storage.sync.set({ isEnabled: false }, () => {
+      console.log("Extension installed - disabled by default");
+      chrome.action.setBadgeText({ text: "" });
+    });
+  }
 });
